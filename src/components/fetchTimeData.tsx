a3486d44ab5b2adb227c632d42ed87ba8e391501
Joshua <joshuapruitt6484@gmail.com>
2025-02-25 11:57:28 -0800

    changed time function to only run once

--- src/components/fetchTimeData.tsx
+++ src/components/fetchTimeData.tsx
@@ -10,30 +10,40 @@ const GetTimeData = () => {
         const res = await axios.get('https://api.ipify.org/?format=json');
         console.log("Ip address:", res.data.ip)
         setIpAddress(res.data.ip)
+
+        return res.data.ip;
     };
 
     // const timer = () => {
     //     setInterval(fetchData, 1000)
     // }
 
-    const fetchData = async () => {
-        const requestLink = `https://timeapi.io/api/time/current/ip?ipAddress=${ip}`;
-        const res = await axios.get(requestLink);
+    const fetchData = async (retIp?: string | null) => {
+        if (retIp){
+            const requestLink = `https://timeapi.io/api/time/current/ip?ipAddress=${retIp}`;
+            const res = await axios.get(requestLink);
 
-        console.log("Time:", res.data);
-        setCurrentTime(res.data);
+            console.log("Time:", res.data);
+            setCurrentTime(res.data);
+        } else {
+            const requestLink = `https://timeapi.io/api/time/current/ip?ipAddress=${ip}`;
+            const res = await axios.get(requestLink);
+
+            console.log("Time:", res.data);
+            setCurrentTime(res.data);
+        }
     }
 
-    useEffect(() => {
-        if(!ip){ //if no ip address then grap ip
-            getIpData();
+    useEffect( () => {
+        const checkIfIp = async () => {
+            if(!ip){ //if no ip address then grap ip
+                const ip = await getIpData();
+                fetchData(ip)
+            }
         }
 
-        if(ip){
-            fetchData();
-        }
-
-    }, [])
+        checkIfIp()
+    })
 
     return (
         <div>
